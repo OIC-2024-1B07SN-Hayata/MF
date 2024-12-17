9cbedd9a2c3e42694b4df9ab803cb3d67d8684eb
竹村優 <c4161@oic.jp>
2024-12-17 17:34:38 +0900

    こんばんは

--- MF/Form1.Designer.cs
+++ MF/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             konnitiwaaa = new Label();
+            label1 = new Label();
             SuspendLayout();
             // 
             // konnitiwaaa
@@ -41,11 +42,22 @@
             konnitiwaaa.Text = "konnnitiwa";
             konnitiwaaa.Click += konnitiwaaa_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(217, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 25);
+            label1.TabIndex = 1;
+            label1.Text = "こんばんは";
+            label1.Click += label1_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Controls.Add(konnitiwaaa);
             Name = "Form1";
             Text = "Form1";
@@ -56,5 +68,6 @@
         #endregion
 
         private Label konnitiwaaa;
+        private Label label1;
     }
 }
